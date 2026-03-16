78be849b5b7d58572f34066d0f41eb16a481b365
emmanueloutreachcentre <emmanuelministriescentre@gmail.com>
2026-03-16 22:19:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,7 @@ self.addEventListener("install", event => {
   console.log("Service Worker installed");
 });
 
+
 self.addEventListener("activate", event => {
   console.log("Service Worker activated");
 });
